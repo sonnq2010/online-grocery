a66cabc71ccaa889af9de7953f933d2da15058ce
sonnq-genki <son-nq@genkisystem.com>
2023-04-10 18:26:34 +0700

    #6 Fix bad url

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:online_grocery/app.dart';
 import 'package:online_grocery/spa_router/spa_router.dart';
 import 'package:online_grocery/pages/base_page/base_controller.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
   // Prepare all needed
@@ -13,6 +14,12 @@ void main() {
 }
 
 void prepare() {
+  // Ensure widget binding is initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // To remove /#/ on the url
+  setPathUrlStrategy();
+
   Get.put(BaseController());
   Get.put(SpaRouter());
 }
