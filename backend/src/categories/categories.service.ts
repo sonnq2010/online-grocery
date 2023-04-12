2e7caf296b854da4c609cc375217a94c5bd5fe2c
TuanNgo2412 <ngohoangtuan1996@gmail.com>
2023-04-12 22:51:16 +0700

    #7 remove code line not use

--- backend/src/categories/categories.service.ts
+++ backend/src/categories/categories.service.ts
@@ -1,42 +1,39 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
 import prisma from 'src/prisma/prisma.service';
 
 @Injectable()
 export class CategoriesService {
+  // Get all categories
+  async findAll() {
+    return await prisma.category.findMany();
+  }
 
-    // Get all categories
-    async findAll() {
-        return await prisma.category.findMany();
-    }
+  // Get a single category by id
+  async findOne(id: string) {
+    return await prisma.category.findUnique({
+      where: { id },
+    });
+  }
 
-    // Get a single category by id
-    async findOne(id: string) {
-        return await prisma.category.findUnique({
-            where: { id },
-        });
-    }
+  // Create a new category
+  async create(data: any) {
+    return await prisma.category.create({
+      data,
+    });
+  }
 
-    // Create a new category
-    async create(data: any) {
-        return await prisma.category.create({
-            data,
-        });
-    }
+  // Update a category by id
+  async update(id: string, data: any) {
+    return await prisma.category.update({
+      where: { id },
+      data,
+    });
+  }
 
-    // Update a category by id
-    async update(id: string, data: any) {
-        return await prisma.category.update({
-            where: { id },
-            data,
-        });
-    }
-
-    // Delete a category by id
-    async remove(id: string) {
-        return await prisma.category.delete({
-            where: { id },
-        });
-    }
+  // Delete a category by id
+  async remove(id: string) {
+    return await prisma.category.delete({
+      where: { id },
+    });
+  }
 }
-
